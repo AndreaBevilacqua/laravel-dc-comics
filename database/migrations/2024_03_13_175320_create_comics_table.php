3f8b29d3946fec0cd609ff81fcdc9debb921c6c0
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-03-14 19:13:34 +0100

    added show and index route

--- database/migrations/2024_03_13_175320_create_comics_table.php
+++ database/migrations/2024_03_13_175320_create_comics_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 100);
+            $table->string('title', 150);
             $table->text('description');
             $table->text('thumb');
             $table->string('price', 7);
-            $table->string('series', 40);
+            $table->string('series', 50);
             $table->date('sale_date');
             $table->string('type', 50);
             $table->string('artists');
